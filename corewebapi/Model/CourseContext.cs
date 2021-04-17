fbfc9a397ac5f06d468317ef49cf33078d2d5e27
Alexander Hernández Herrera <aherrera@itcoint.com>
2021-04-16 18:43:48 -0600

    Agregar archivos de proyecto.

---
+++ corewebapi/Model/CourseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace corewebapi.Model
+{
+    public class CourseContext : DbContext
+    {
+        // Replace your connection string here
+        readonly string connectionstring = "Server=tcp:demo89000.database.windows.net,1433;Initial Catalog=pruebaconalex;Persist Security Info=False;User ID=demouser;Password=Demo123456789-;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(connectionstring);
+            base.OnConfiguring(options);
+        }
+    }
+
+    
+}
